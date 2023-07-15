11090bc11eac166a3af1d0774ebde01f67b09803
johnsonafool <johnsonafool@gmail.com>
2023-07-15 11:31:43 +0800

    feat: pages

---
+++ src/app/api/pages/route.ts
@@ -0,0 +1,57 @@
+import {
+  NextResponse,
+  type NextFetchEvent,
+  type NextRequest,
+} from "next/server";
+import { string, z } from "zod";
+import { extractBody } from "@/lib/extract-body";
+import prisma from "@/lib/connect-prisma";
+
+export const config = {
+  runtime: "edge",
+};
+
+const schema = z.object({
+  handle: z.string().max(64).min(1),
+});
+
+async function createPageHandler(req: NextRequest, event: NextFetchEvent) {
+  let handle = "";
+  const body = await extractBody(req);
+
+  try {
+    const { handle: pageHandle } = schema.parse(body);
+    handle = pageHandle;
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      console.log(err.issues);
+    }
+  }
+
+  try {
+    await prisma.page.create({
+      data: {
+        handle
+      },
+    });
+
+    return new Response(JSON.stringify({ handle }), {
+      status: 200,
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response("something went wrong", {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(req: NextRequest, event: NextFetchEvent) {
+  if (req.method !== "POST") {
+    return new Response("invalid method", {
+      status: 405,
+    });
+  }
+
+  return createPageHandler(req, event);
+}
---
+++ src/lib/get-query-client.ts
@@ -0,0 +1,5 @@
+import { QueryClient } from "@tanstack/query-core";
+import { cache } from "react";
+
+export const getQueryClient = cache(() => new QueryClient());
+
